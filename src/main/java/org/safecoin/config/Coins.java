3114aaaedb6aa3743e4b89b787e3c7e4e6ec009e
EternityTula <tu1220t01@mail.ru>
2019-06-15 12:35:45 +0300

    add coins

--- src/main/java/org/safecoin/config/Coins.java
+++ src/main/java/org/safecoin/config/Coins.java
@@ -2,9 +2,12 @@ package org.safecoin.config;
 
 public enum Coins {
     SAFE("SafeCoin", RateDataSource.SAFE_TRADE),
+    RITO("Ritocoin", RateDataSource.SAFE_TRADE),
     BTCZ("BitcoinZ", RateDataSource.COINLIB),
+    BTG("Bitcoin Golg", RateDataSource.COINLIB),
     ANON("Anonymous Bitcoin", RateDataSource.SAFE_TRADE),
     ZEL("ZelCash", RateDataSource.COINLIB),
+    ZEN("HoriZen", RateDataSource.COINLIB),
     RVN("Ravencoin", RateDataSource.COINLIB),
     ZCL("Zclassic", RateDataSource.COINLIB),
     LTC("Litecoin", RateDataSource.COINLIB),
