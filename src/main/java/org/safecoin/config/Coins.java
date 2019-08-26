d8938ceb75b403bdd196b5e7c6039c29f6c622e9
EternityTula <tu1220t01@mail.ru>
2019-08-26 10:12:09 +0300

    Adding DOGE XSG
    Sorting coins alphabetically by name and adding DOGE, XSG.

--- src/main/java/org/safecoin/config/Coins.java
+++ src/main/java/org/safecoin/config/Coins.java
@@ -2,19 +2,21 @@ package org.safecoin.config;
 
 public enum Coins {
     SAFE("SafeCoin", RateDataSource.SAFE_TRADE),
-    RITO("Ritocoin", RateDataSource.SAFE_TRADE),
+    ANON("Anonymous Bitcoin", RateDataSource.SAFE_TRADE),
+    BCH("Bitcoin Cash", RateDataSource.COINLIB),
+    BC("Bitcoin Confidential", RateDataSource.SAFE_TRADE),
     BTCZ("BitcoinZ", RateDataSource.COINLIB),
     BTG("Bitcoin Gold", RateDataSource.COINLIB),
-    BC("Bitcoin Confidential", RateDataSource.SAFE_TRADE),    
-    ANON("Anonymous Bitcoin", RateDataSource.SAFE_TRADE),
-    ZEC("Zcash", RateDataSource.COINLIB),
-    YEC("Ycash", RateDataSource.SAFE_TRADE),
-    ZEL("ZelCash", RateDataSource.COINLIB),
+    DOGE("Dogecoin", RateDataSource.COINLIB),
     ZEN("HoriZen", RateDataSource.COINLIB),
-    RVN("Ravencoin", RateDataSource.COINLIB),
-    ZCL("Zclassic", RateDataSource.COINLIB),
     LTC("Litecoin", RateDataSource.COINLIB),
-    BCH("Bitcoin Cash", RateDataSource.COINLIB);
+    RVN("Ravencoin", RateDataSource.COINLIB),
+    RITO("Ritocoin", RateDataSource.SAFE_TRADE),
+    XSG("Snowgem", RateDataSource.COINLIB),
+    YEC("Ycash", RateDataSource.SAFE_TRADE),
+    ZEC("Zcash", RateDataSource.COINLIB),
+    ZCL("Zclassic", RateDataSource.COINLIB),
+    ZEL("ZelCash", RateDataSource.COINLIB);
 
     private String name;
 
